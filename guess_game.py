80b700d9ca9cbced10e19136f690c23ff79e51db
Mikolaj Olszowiec <mikolajolszowiec@gmail.com>
2019-09-08 16:15:02 +0200

    implemented functionality add number

--- guess_game.py
+++ guess_game.py
@@ -1,6 +1,11 @@
+from random import randint 
+
 def get_user_range():
 	print("Podaj ziomeczku minimum zakresu")
 	min_r = int(input())
 	print("Podaj ziomeczku maksimum zakresu")
 	max_r = int(input())
 	return min_r, max_r
+
+def random_from_range(min_d, max_d):
+	return randint(min_d, max_d)
